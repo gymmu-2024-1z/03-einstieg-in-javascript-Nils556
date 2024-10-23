f8d8787ebf86c381db1d414b4052dc7432c47953
Nils <e227368@edu.sbl.ch>
2024-10-23 14:16:24 +0000

    Aufgabe 3 und 4

--- script.js
+++ script.js
@@ -66,11 +66,30 @@ export function aufgabe03(args) {
   const input = args
   const result = []
 
+  let count = 0
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
   }
 
-  return result.join("")
+  return count
 }
 
 linkupExerciseHandler("[data-click=aufgabe03]", aufgabe03)
+
+export function aufgabe04(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "word") count = count + 1
+  }
+
+  return count
+}
